1c32b82f1ee35f5fdbd406d8ee8767e740f72c0f
Prabhudas-M <60952658+Prabhudas-M@users.noreply.github.com>
2020-12-10 08:19:26 -0500

    class

--- src/components/ToDoItem.jsx
+++ src/components/ToDoItem.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-// function ToDoItem(props) {
-//   return <li>{props.text}</li>;
-// }
-
-class ToDoItem extends React.Component {
-  render() {
-    return <li>{this.props}</li>;
-  }
+function ToDoItem(props) {
+  return <li>{props.text}</li>;
 }
+
+// class ToDoItem extends React.Component {
+//   render() {
+//     return <li>{this.props.text}</li>;
+//   }
+// }
 export default ToDoItem;
